964ec7ab1614dd0f2a14124b6eeb9db32ad4193c
[sharoun <[madoyasharoun@gmail.com]>
2023-06-22 11:10:53 +0300

    Complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,38 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL ='https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(URL)
+        return response.content
 
     def load_json(self):
-        pass
+        name_list = []
+
+        names= json.loads(self.get_response_body())
+        for name in names:
+            name_list.append(name['name'])
+            return names
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        #     response_body = self.get_response_body()
+        # json_data = json.loads(response_body)
+        # return json_data
